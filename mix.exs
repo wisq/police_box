61763d51b567b4b4af037be6cfbe605138b85329
Adrian Irving-Beer <wisq@wisq.net>
2023-06-15 12:48:07 -0400

    Add testing dependencies.

--- mix.exs
+++ mix.exs
@@ -27,7 +27,9 @@ defmodule PoliceBox.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:pi_glow, "~> 0.1.0"}
+      {:pi_glow, "~> 0.1.0"},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:ex_git_test, "~> 0.1.2", only: [:dev, :test], runtime: false}
     ]
   end
 end
